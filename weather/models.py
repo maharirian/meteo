666be98d037bbcd34cf8d518a9899fa11bd986ab
mah <ma.haririan1383@gmail.com>
2026-02-06 23:55:22 +0330

    add model

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    title = models.CharField(max_length=50)
+    lat = models.FloatField()
+    lon = models.FloatField()
