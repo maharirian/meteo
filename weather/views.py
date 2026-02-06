e2d006e36332f0710cd202dc9f40a69f7faf3c73
mah <ma.haririan1383@gmail.com>
2026-02-06 20:01:03 +0330

    add api

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-import random
+import requests
 
 def show_temp_view(request):
-    temp = random.randint(1,30)
+    lat, lon = 37.7981, 48.5146
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    try:
+        data = requests.get(url).json()
+        temp = data['current_weather']['temperature']
+    except Exception:
+        temp = None
     context = {"city":"hamedan","temp":temp}
     return render(request ,"show_temp.html",context)
