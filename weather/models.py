72899796c35c4eec73a699429e44700c3e99afc0
mah <ma.haririan1383@gmail.com>
2026-02-07 00:22:23 +0330

    add city with admin panel

--- weather/models.py
+++ weather/models.py
@@ -4,3 +4,5 @@ class City(models.Model):
     title = models.CharField(max_length=50)
     lat = models.FloatField()
     lon = models.FloatField()
+    def __str__(self):
+        return self.title
