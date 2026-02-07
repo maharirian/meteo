48989652a5444d7a407fd41d0cf617fdab8aa565
mah <ma.haririan1383@gmail.com>
2026-02-07 14:59:32 +0330

    work with orm

--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 def show_temp_view(request):
-    lat, lon = 37.7981, 48.5146
+    city = City.objects.all().first()
+    lat, lon = city.lat, city.lon
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
     try:
         data = requests.get(url).json()
         temp = data['current_weather']['temperature']
     except Exception:
         temp = None
-    context = {"city":"hamedan","temp":temp}
+    context = {"city":city.title,"temp":temp}
     return render(request ,"show_temp.html",context)
